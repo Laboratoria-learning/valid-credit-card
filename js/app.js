69486f20a35605c5aec99950522aa1cfd09c19f1
yennyelizabeth <31896385+yennyelizabeth@users.noreply.github.com>
2017-11-03 11:55:57 -0500

    añadiendo ajustes a la identación

--- js/app.js
+++ js/app.js
@@ -13,21 +13,18 @@ do {
 
      var tarjet= prompt('Ingrese Número de Tarjeta: ') ; // se solicita nro de tarjeta de crédito
      var n= tarjet.length;// captura el tamaño de longitud de la variable tarjet
-     //sentence = prompt('ingrese frase: ');
 
      if (tarjet.length ===0){
         alert('No ingreso Número de Tarjeta:') ;
         var validate=false
      }else{
-
          count=0;
-
          for (var a=0; a< tarjet.length ; a++){
 
-           if(tarjet.charCodeAt(a)>=48 & tarjet.charCodeAt(a)<=57 ) {
-              validate=true;
-              count= count+1;
-           }
+            if(tarjet.charCodeAt(a)>=48 & tarjet.charCodeAt(a)<=57 ) {
+               validate=true;
+               count= count+1;
+            }
          }
 
          if( count === tarjet.length){
@@ -47,39 +44,39 @@ if (validate=== true){
 
 // Función Validación de Tarjeta
 function isValidCard ( tarjet){
+     
+     // utilizar un for para armar un arrayInitial invertida
+     for (var i=0 ; i<n ; i++){
+         arrayInitial[i+1]= parseInt(tarjet.substr(i,1));
+     }
 
-// utilizar un for para armar un arrayInitial invertida
-for (var i=0 ; i<n ; i++){
-    arrayInitial[i+1]= parseInt(tarjet.substr(i,1));
-}
+     // utilizar un for para armar un arrayResult a traves del arrayInitial
+     for (var j=1; j <= tarjet.length; j++){
 
-// utilizar un for para armar un arrayResult a traves del arrayInitial
-for ( var j=1; j <= tarjet.length; j++){
+         // utilizo un if para identificar posicion par o impar
+         if (j %2 !== 0){
+            arrayResult[j]= arrayInitial[j] ; //valores de posiciones impares
+            acumulate +=  arrayResult[j]; // acumula valor posicion impar
+         }  else {
+            tempNumber = arrayInitial[j]*2
 
-    // utilizo un if para identificar posicion par o impar
-    if ( j %2 !== 0){
-       arrayResult[j]= arrayInitial[j] ; //valores de posiciones impares
-       acumulate +=  arrayResult[j]; // acumula valor posicion impar
-    } else {
-      tempNumber = arrayInitial[j]*2
-
-    // utilizo if para controlar la divisibilidad entre 10
-      if ( tempNumber >=10){
-         tempString = tempNumber.toString();
-         arrayResult[j] = parseInt(tempString.substr(0,1))+parseInt(tempString.substr(1,1)); //valores posiciones pares segun operacion >=10
-         acumulate +=  arrayResult[j]; // acumula valor posicion par
-      } else{
-        arrayResult[j]= arrayInitial[j] // valores de posiciones pares <10
-        acumulate += arrayResult[j];  // acumula valor posicion par
+            // utilizo if para controlar la divisibilidad entre 10
+              if (tempNumber >=10){
+                   tempString = tempNumber.toString();
+                   arrayResult[j] = parseInt(tempString.substr(0,1))+parseInt(tempString.substr(1,1)); //valores posiciones pares segun operacion >=10
+                   acumulate +=  arrayResult[j]; // acumula valor posicion par
+              }  else{
+                   arrayResult[j]= arrayInitial[j] // valores de posiciones pares <10
+                   acumulate += arrayResult[j];  // acumula valor posicion par
+              }
+         }
       }
-    }
-}
 
-  if ( acumulate%10 ===0){
-    alert('Tarjeta de Credito SI Válida');
+      if (acumulate%10 ===0){
+         alert('Tarjeta de Crédito SI Válida');
 
-  } else {
-    alert('Tarjeta de Credito NO Válida');
-    alert( 'Valor Acumulado:   '+acumulate);
-  }
+      } else {
+         alert('Tarjeta de Crédito NO Válida');
+         alert( 'Valor Acumulado:   '+acumulate);
+      }
 }
