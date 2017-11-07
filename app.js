c6ed4c2992c86a82fbad388363d1c6be6ae337fc
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-07 17:18:03 -0500

    corrigiendo indentación

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-function isValidCard(number){
-    if (number === ''){
-        alert('No puede dejar este campo vacio');
+function isValidCard(number) {
+  if (number === '') {
+    alert('No puede dejar este campo vacio');
+  }
+  // guardaremos el número invertido
+  var arr = [];
+  for (var i = number.length - 1; i >= 0; i--) {
+    arr.push(parseInt(number.charAt(i)));
+  }
+  // recorremos el array
+  for (var i = 0; i < arr.length; i++) {
+    // indice impar -- para la posición par 
+    if (i % 2 === 1) {
+      var numberPar = arr[i] * 2;
+      if (numberPar >= 10) {
+        // convertir a string
+        var sum = numberPar.toString();
+        // con parseInt convertir a número
+        var newNumber = parseInt(sum.charAt(0)) + parseInt(sum.charAt(1));
+        arr[i] = newNumber;
+      } else {
+        arr[i] = numberPar;
+      }
     }
-    var arr = [];//guardaremos el numero invertido
-    for (var i = number.length - 1; i >= 0; i--){
-        arr.push(parseInt(number.charAt(i)));//
-    }
-    //recorremos el array
-    for (var i = 0; i < arr.length; i++){//La primera vuelta no va entrar
-        //indice impar por logica entrara para la posicion par 
-        if (i  % 2 === 1){
-            var numberPar = arr[i] * 2;
-            if (numberPar >= 10){
-                //convierto a string
-                var sum = numberPar.toString();
-                //con parseInt convierto a numero
-                var newNumber = parseInt(sum.charAt(0)) + parseInt(sum.charAt(1))
-                arr[i] = newNumber;
-            }else {
-                arr[i] = numberPar
-            }
-        }
-    }
-    var total = 0;
-    for (var i = 0; i < arr.length; i++){
-        total += arr[i];
-    }
-    if (total % 10 === 0){
-        return('Tarjeta valida');
-    }else {
-        return ('Tarjeta invalida');
+  }
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  if (total % 10 === 0) {
+    return ('Tarjeta válida');
+  } else {
+    return ('Tarjeta inválida');
   }
 }
-var number = prompt('Introduce el numero de tarjeta');
-console.log(isValidCard(number));
+var number = prompt('Introduce el número de tarjeta');
+alert(isValidCard(number));
