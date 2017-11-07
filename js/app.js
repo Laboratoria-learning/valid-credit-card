112a5438e5383005cd6078b56286324236afa9b6
YaninaMR <mercadoyanic@gmail.com>
2017-11-07 18:26:20 -0500

    add complete files

--- js/app.js
+++ js/app.js
@@ -1,51 +1,37 @@
-﻿var n = prompt("Ingrese su Nro de Tarjeta de credito");
-function  isValidCard(n){
-    var val =/^[0-9]*$/;
-    if(n ==="" || (!val.test(n)) ){
-     do{
-       var m = prompt("Esta vacio o solo debe contener digitos de 0-9,Por favor vuelva a ingresar ");    
-       
-     }while(!m || !val.test(m) );
-     n = m;
-   }
-
-
-
-/*var arr = [];
-for(i=0;i<(n.length);i++){
-     arr.push(parseInt(n[i]));
-}*/
-  var arr = n.split("");
+﻿var num = prompt('Input credit number');
+function isValidCard(num) {
+  // valid credit number
+  var val = /^[0-9]*$/;
+  if (num === '' || (!val.test(num))) {
+    do {
+      var numValid = prompt('It´s empty or don´t contain digits between 0-9,Please  input again');
+    } while (!numValid || !val.test(numValid));
+    num = numValid;
+  }
+  // convert creditCard in  numbers array
+  var arr = num.split('');
   for (var i = 0; i < arr.length; i++) {
     var x = parseInt(arr.pop());
     arr.splice(i, 0, x);
   }
-
-
-
-var sum = arr[arr.length-1];
-for(i=0;i<(arr.length-1);i++){
-          if((i+1)%2 === 0){
-            p1=  arr[i]*2; 
-          //  
-                     if (p1>9){
-                         c2 = parseInt(p1/10);
-                         r2 = parseInt(p1%10); 
-                         p2= c2+r2;
-                      }else {
-                      p2=p1;}
-            arr.splice(i,1,p2) ; 
-            
-            }
-       sum = sum+arr[i];
+  // valid ciphers, credit card and addition digits credit card 
+  var sum = arr[arr.length - 1];
+  for (i = 0;i < (arr.length - 1);i++) {
+    if ((i + 1) % 2 === 0) {
+      p1 = arr[i] * 2;
+      if (p1 > 9) {
+        c2 = parseInt(p1 / 10);
+        r2 = parseInt(p1 % 10);
+        p2 = c2 + r2;
+      } else {
+        p2 = p1;
+      }
+      arr.splice(i, 1, p2) ;
     }
+    sum = sum + arr[i];
+  }
+  // return  valid or not valid and the total sum
+  return (sum % 10 === 0) ? (alert(num + ': Valid CreditCard ' + ', Total sum :' + sum)) : (alert(num + ': invalid CreditCard' + ',Total sum :' + sum));
+}
 
-   
-    if(sum%10=== 0){
-      alert(n +": Tarjeta valida " + ", suma : " +sum)
-    }else{
-      alert(n +": Tarjeta invalida"+ ", suma :" +sum)
-    }
-}    
-
-isValidCard(n);
+isValidCard(num);
